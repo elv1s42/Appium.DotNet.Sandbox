6a9eeb348750038ddb3bb9c629b689c42c7ae46c
Evgeniy Kosjakov <42_@mail.ru>
2016-07-25 02:05:18 +0300

    new check

--- Appium.Net.Sandbox/MainRunner.cs
+++ Appium.Net.Sandbox/MainRunner.cs
@@ -21,7 +21,8 @@ namespace Appium.Net.Sandbox
             capabilities.SetCapability("platformName", "Android");
             capabilities.SetCapability("platformVersion", "6.0 Marshmallow");
             capabilities.SetCapability("app", "com.instagram.android");
-            capabilities.SetCapability("appActivity", "com.instagram.android.activity.MainTabActivity");
+            capabilities.SetCapability("appActivity", "com.instagram.android.activity.MainTabActivity"); 
+            capabilities.SetCapability("unicodeKeyboard", "true");
 
             Driver = new RemoteWebDriver(
                 new Uri("http://192.168.1.101:4723/wd/hub"), capabilities, TimeSpan.FromSeconds(30));
@@ -31,9 +32,20 @@ namespace Appium.Net.Sandbox
         [Test]
         public void Check()
         {
+            string hashs;
             Driver
                 .OpenProfile()
-                .OpenPhoto(1, 3)
+                .OpenPhoto(1, 1)
+                .EditPhoto()
+                .ClearHashtags(out hashs)
+                .SaveEditing()
+                .GoBack()
+                .OpenPhoto(1, 1)
+                .EditPhoto()
+                .SetHashtags(hashs)
+                //.SetHashtags("#закат #питер #санктпетербург #спб")
+                //.SetHashtags("123")
+                .SaveEditing()
                 .GoBack();
         }
 
--- Appium.Net.Sandbox/Pages/PhotoPageActions.cs
+++ Appium.Net.Sandbox/Pages/PhotoPageActions.cs
@@ -1,5 +1,9 @@
-﻿using Appium.Net.Sandbox.Utils;
+﻿using System;
+using System.Linq;
+using System.Threading;
+using Appium.Net.Sandbox.Utils;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Interfaces;
 
 namespace Appium.Net.Sandbox.Pages
 {
@@ -20,6 +24,64 @@ namespace Appium.Net.Sandbox.Pages
             {
                 d.FindElement(By.Id("com.instagram.android:id/media_option_button"))
                 .Click();
+            });
+        }
+
+        public static IWebDriver ClickEdit(this IWebDriver d)
+        {
+            return d.DefaultAction("Clicking Edit...", () =>
+            {
+                d.FindElements(By.Id("com.instagram.android:id/row_simple_text_textview"))
+                .First(e => e.Text.Equals("Edit"))
+                .Click();
+            });
+        }
+
+        public static IWebDriver EditPhoto(this IWebDriver d)
+        {
+            return d.ClickParameters().ClickEdit();
+        }
+
+        public static IWebDriver ClearHashtags(this IWebDriver d, out string hashs)
+        {
+            var t = d.FindElement(By.Id("com.instagram.android:id/edit_media_caption"))
+                   .Text;
+            hashs = t;
+            return d.DefaultAction($"Clearing hashtags: '{hashs}' ...", () =>
+            {
+                d.FindElement(By.Id("com.instagram.android:id/edit_media_caption")).Clear();
+            });
+        }
+
+        public static IWebDriver SetHashtags(this IWebDriver d, string hashs)
+        {
+            return d.DefaultAction($"Setting hashtags: '{hashs}'...", () =>
+            {
+                var e = d.FindElement(By.Id("com.instagram.android:id/edit_media_caption"));
+                e.Click();
+                Thread.Sleep(500);
+                e.SendKeys(hashs);
+                Thread.Sleep(1000);
+                d.FindElement(By.Id("com.instagram.android:id/action_bar_textview_title")).Click();
+                
+            });
+        }
+
+        public static IWebDriver CloseEditing(this IWebDriver d)
+        {
+            return d.DefaultAction("Closing Edit...", () =>
+            {
+                d.FindElement(By.Id("com.instagram.android:id/action_bar_button_back"))
+                    .Click();
+            });
+        }
+
+        public static IWebDriver SaveEditing(this IWebDriver d)
+        {
+            return d.DefaultAction("Saving Edit...", () =>
+            {
+                d.FindElement(By.Id("com.instagram.android:id/action_bar_button_action"))
+                    .Click();
             });
         }
     }
