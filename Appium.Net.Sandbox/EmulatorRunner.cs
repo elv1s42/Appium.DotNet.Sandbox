02a5bbf1528b5d4ff0480c48415184345ae94398
Evgeniy Kosjakov <42_@mail.ru>
2016-07-30 23:19:57 +0300

    addition

--- Appium.Net.Sandbox/EmulatorRunner.cs
+++ Appium.Net.Sandbox/EmulatorRunner.cs
@@ -1,7 +1,72 @@
-﻿namespace Appium.Net.Sandbox
+﻿using System;
+using Appium.Net.Sandbox.Pages;
+using Appium.Net.Sandbox.Utils;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Remote;
+
+namespace Appium.Net.Sandbox
 {
-    public class EmulatorRunner
+    public static class EmulatorRunner
     {
-         
+        public static RemoteWebDriver Driver;
+
+        public static DesiredCapabilities DesiredCapabilities
+        {
+            get
+            {
+                var capabilities = new DesiredCapabilities();
+                capabilities.SetCapability("device", "Android");
+                capabilities.SetCapability("deviceName", "Nexus 5");
+                capabilities.SetCapability("platformName", "Android");
+                capabilities.SetCapability("platformVersion", "6.0 Marshmallow");
+                capabilities.SetCapability("app", "com.instagram.android");
+                capabilities.SetCapability("appActivity", "com.instagram.android.activity.MainTabActivity");
+                capabilities.SetCapability("unicodeKeyboard", "true");
+                return capabilities;
+            }
+        }
+
+        public static void SetUp()
+        {
+            Driver = new AndroidDriver<AppiumWebElement>(
+                       new Uri("http://192.168.1.101:4723/wd/hub"),
+                       DesiredCapabilities, TimeSpan.FromSeconds(30));
+        }
+
+        public static void End()
+        {
+            Driver.Dispose();
+        }
+
+        public static void ResetAllHashs(int row, int column)
+        {
+            try
+            {
+                ConsoleLogger.Write($"Updating photo: row={row}, column={column}");
+                SetUp();
+                string hashs;
+                Driver
+                    .OpenProfile()
+                    .OpenPhoto(row, column)
+                    .EditPhoto()
+                    .ClearHashtags(out hashs)
+                    .SaveEditing()
+                    .GoBack()
+                    .OpenPhoto(row, column)
+                    .EditPhoto()
+                    .SetHashtags(hashs)
+                    .SaveEditing()
+                    .GoBack();
+                End();
+            }
+            catch (Exception e)
+            {
+                ConsoleLogger.Message("Error for Resetting hashs!!!");
+                ConsoleLogger.Message(e.Message);
+                ConsoleLogger.Message(e.StackTrace);
+            }
+        }
     }
 }
+
---
+++ Appium.Net.Sandbox/Pages/Steps.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Appium.Net.Sandbox.Utils;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+
+namespace Appium.Net.Sandbox.Pages
+{
+    public static class Steps
+    {
+        public static void ResetAllHashs(this AndroidDriver<AppiumWebElement> d, int row, int column)
+        {
+            try
+            {
+                ConsoleLogger.Write($"Updating photo: row={row}, column={column}");
+                string hashs;
+                d
+                    .OpenProfile()
+                    .OpenPhoto(row, column)
+                    .EditPhoto()
+                    .ClearHashtags(out hashs)
+                    .SaveEditing()
+                    .GoBack()
+                    .OpenPhoto(row, column)
+                    .EditPhoto()
+                    .SetHashtags(hashs)
+                    .SaveEditing()
+                    .GoBack();
+            }
+            catch (Exception e)
+            {
+                ConsoleLogger.Message("Error for Resetting hashs!!!");
+                ConsoleLogger.Message(e.Message);
+                ConsoleLogger.Message(e.StackTrace);
+            }
+        }
+    }
+}
